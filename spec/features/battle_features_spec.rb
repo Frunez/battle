ac2605110468abf1bd704a418841dac9d7145c23
Frunez <francesco.nunez@gmail.com>
2016-07-21 15:02:29 +0100

    21. created web helpers with method sign_in_and_play

--- spec/features/battle_features_spec.rb
+++ spec/features/battle_features_spec.rb
@@ -2,10 +2,14 @@
 
 RSpec.feature "Create players", :type => :feature do
   scenario "creates 2 players" do
-    visit "/"
-    fill_in :player_1, with: "Zeeshan"
-    fill_in :player_2, with: "Frankie"
-    click_button "submit"
+    sign_in_and_play
     expect(page).to have_content("Zeeshan vs. Frankie")
+  end
+end
+
+feature "Hitpoints" do
+  scenario "view player health" do
+    sign_in_and_play
+    expect(page).to have_content("Frankie has 100hp")
   end
 end
