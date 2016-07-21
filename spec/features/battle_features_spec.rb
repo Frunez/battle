9c3b7e6f1a2cc84c9a272ee3fbf81c31c8e5ede4
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-21 15:16:13 +0100

    player one attacks player two

--- spec/features/battle_features_spec.rb
+++ spec/features/battle_features_spec.rb
@@ -1,6 +1,4 @@
-
-
-RSpec.feature "Create players", :type => :feature do
+feature "Create players" do
   scenario "creates 2 players" do
     sign_in_and_play
     expect(page).to have_content("Zeeshan vs. Frankie")
@@ -11,5 +9,13 @@ feature "Hitpoints" do
   scenario "view player health" do
     sign_in_and_play
     expect(page).to have_content("Frankie has 100hp")
+  end
+end
+
+feature 'attack players' do
+  scenario "attack player two and get confirmation" do
+    sign_in_and_play
+    click_link("attack")
+    expect(page).to have_content("Zeeshan attacked Frankie")
   end
 end
