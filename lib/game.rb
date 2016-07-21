f9e0140a42447d1b86101a02f583cef881200c8f
Frunez <francesco.nunez@gmail.com>
2016-07-21 18:34:13 +0100

    switching player turns (unfinished)

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,26 @@
 class Game
 
-  attr_reader :attack
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @current_turn = player_1
+  end
 
   def attack(player)
     player.receive_damage
   end
 
+  def switch_turns(player)
+    if @current_turn == player_1
+      @current_turn = player_2
+    elsif @current_turn == player_2
+      @current_turn = player_1
+  end
+
+  def opponent(player)
+    switch_turns(player)
+    @current_turn
+  end
 end
